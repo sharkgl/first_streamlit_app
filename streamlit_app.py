7389ca8821fd9c1aad51525bab079123a8fe40d8
sharkgl <131492792+sharkgl@users.noreply.github.com>
2023-10-12 12:50:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-steamlit.text('Kale, Spinach & Rocket Smoothie')
-steamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
